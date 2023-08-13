47aed7c71d37515296eacbe57a284363c659d4e1
Martin Burger <martin.burger@htwsaar.de>
2023-08-13 19:33:11 +0200

    [Code] fix Checkstyle warnings

---
+++ src/main/java/net/martinburger/sesqa/programming/App.java
@@ -0,0 +1,16 @@
+package net.martinburger.sesqa.programming;
+
+/**
+ * Simple <code>Hello world!</code> application.
+ *
+ */
+public final class App {
+
+  private App() {
+    // not called - utility classes should not have a public or default constructor
+  }
+
+  public static void main(final String[] args) {
+    System.out.println("Hello World!");
+  }
+}
